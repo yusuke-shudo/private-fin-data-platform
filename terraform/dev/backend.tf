9556e3dbb5bdfbb969068519f1174fd9507db15a
yusuke-shudo-member <y5kj757+system-member@gmail.com>
2026-06-13 22:30:26 +0900

    terraform init時、必要な情報を外から注入するように修正。

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -2,7 +2,5 @@ terraform {
   backend "s3" {
     key    = "infrastructure/terraform.tfstate"
     region = "ap-northeast-1"
-    use_s3_express_and_native_locking = true
-    encrypt = true
   }
 }
