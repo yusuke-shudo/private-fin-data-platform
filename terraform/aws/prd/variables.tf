c03c7abe452102cc17e77d708848a358ecf363f1
yusuke-shudo-member <y5kj757+system-member@gmail.com>
2026-06-17 20:31:39 +0900

    prd環境もdevとsnowflakeを分割する、など

---
+++ terraform/aws/prd/variables.tf
@@ -0,0 +1,14 @@
+variable "env" {
+  type    = string
+  description = "実行環境(dev または prd)"
+}
+
+variable "sf_user_arn" {
+  type        = string
+  description = "SnowflakeのStorage Integrationから発行されるAWS用ユーザーARN"
+}
+
+variable "sf_external_id" {
+  type        = string
+  description = "SnowflakeのStorage Integrationから発行される外部ID"
+}
