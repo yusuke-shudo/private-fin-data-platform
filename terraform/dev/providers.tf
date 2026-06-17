ec9dc82f76fddc99287945168387ff669067b709
yusuke-shudo-member <y5kj757+system-member@gmail.com>
2026-06-17 11:57:42 +0900

    Snowflakeと連携するためのS3バケット作成テスト

--- terraform/dev/providers.tf
+++ terraform/dev/providers.tf
@@ -13,11 +13,17 @@ terraform {
   }
 }
 
+data "aws_caller_identity" "current" {}
+
 # =========================================================================
 # 1. AWS Provider 設定
 # =========================================================================
 provider "aws" {
   region = "ap-northeast-1"
+  assume_role {
+    role_arn     = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/github-actions-resource-creation-role"
+    session_name = "TerraformDeployment"
+  }
 }
 
 # =========================================================================
