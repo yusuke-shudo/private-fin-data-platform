dc9b22c0e215f7ac75f401054d7b3ffe566a0a02
yusuke-shudo-member <y5kj757+system-member@gmail.com>
2026-06-22 15:15:14 +0900

    新バージョン用output対応

--- terraform/snowflake/dev/outputs.tf
+++ terraform/snowflake/dev/outputs.tf
@@ -1,9 +1,7 @@
 output "storage_aws_iam_user_arn" {
-  description = "Copy this value to GitHub Variables: SF_USER_ARN"
-  value       = [for attr in snowflake_storage_integration_aws.s3_integration.describe_output : attr.value if attr.name == "STORAGE_AWS_IAM_USER_ARN"][0]
+  value = snowflake_storage_integration_aws.s3_integration.describe_output[0].iam_user_arn
 }
 
 output "storage_aws_external_id" {
-  description = "Copy this value to GitHub Variables: SF_EXTERNAL_ID"
-  value       = [for attr in snowflake_storage_integration_aws.s3_integration.describe_output : attr.value if attr.name == "STORAGE_AWS_EXTERNAL_ID"][0]
+  value = snowflake_storage_integration_aws.s3_integration.describe_output[0].external_id
 }
