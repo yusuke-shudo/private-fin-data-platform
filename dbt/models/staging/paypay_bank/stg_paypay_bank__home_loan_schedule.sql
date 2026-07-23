b3cfc81bd61f6fe79f1733f7e6531c93ac68e090
EC2 Default User <ec2-user@ip-10-120-10-171.ap-northeast-1.compute.internal>
2026-07-23 06:01:00 +0000

    Add dbt staging models and config updates

---
+++ dbt/models/staging/paypay_bank/stg_paypay_bank__home_loan_schedule.sql
@@ -0,0 +1,30 @@
+WITH source_data AS (
+  SELECT * FROM {{ source('paypay_bank', 'home_loan_schedule_raw') }}
+),
+
+csv_split AS (
+  SELECT
+    SPLIT(REPLACE(raw_text, '"'), ',') AS col_array,
+    ingest_at_utc
+  FROM
+    source_data
+  WHERE
+    line_number > 1
+),
+
+final AS (
+  SELECT
+    TO_DATE(col_array[0]::VARCHAR, 'YYYY/MM/DD') AS payment_date,
+    col_array[1]::NUMBER AS payment_amount,
+    col_array[2]::NUMBER AS principal_amount,
+    col_array[3]::NUMBER AS interest_amount,
+    NULLIF(col_array[4]::VARCHAR, '-')::NUMBER AS extra_principal_amount,
+    NULLIF(col_array[5]::VARCHAR, '-')::NUMBER AS extra_interest_amount,
+    col_array[6]::NUMBER(4, 2) AS annual_interest_rate,
+    col_array[7]::NUMBER AS remaining_balance,
+    ingest_at_utc
+  FROM
+    csv_split
+)
+
+SELECT * FROM final;
