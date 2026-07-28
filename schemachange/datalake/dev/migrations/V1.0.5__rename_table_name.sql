57caa44c35c377836daf5692ce4e1019bf8bd374
EC2 Default User <ec2-user@ip-10-120-10-104.ap-northeast-1.compute.internal>
2026-07-28 11:38:59 +0000

    雑多な修正

--- schemachange/datalake/dev/migrations/V1.0.5__rename_table_name.sql
+++ schemachange/datalake/dev/migrations/V1.0.5__rename_table_name.sql
@@ -7,7 +7,7 @@ ALTER TABLE datalake_db.sbi_securities.sbi_tokutei_profit_loss_report_raw
   DROP CONSTRAINT pk_sbi_tokutei_profit_loss_report_raw;
 
 ALTER TABLE datalake_db.sbi_securities.sbi_tokutei_profit_loss_report_raw
-  RENAME TO tokutei_profit_loss_report_raw;
+  RENAME TO datalake_db.sbi_securities.tokutei_profit_loss_report_raw;
 
 ALTER TABLE datalake_db.sbi_securities.tokutei_profit_loss_report_raw
   ADD CONSTRAINT pk_tokutei_profit_loss_report_raw PRIMARY KEY (file_path, line_number);
