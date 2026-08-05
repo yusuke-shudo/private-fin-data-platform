105bd638920eba0f0ef2e428255ec05e2dd943e6
EC2 Default User <ec2-user@ip-10-120-10-102.ap-northeast-1.compute.internal>
2026-08-05 16:52:40 +0000

    orico card stating処理作成

---
+++ dbt/tests/orico_credit/stg_orico_credit__home_reform_loan_schedule__interest_amount_matches_formula.sql
@@ -0,0 +1,44 @@
+WITH stg_data AS (
+  SELECT * FROM {{ ref('stg_orico_credit__home_reform_loan_schedule') }}
+),
+
+ordered AS (
+  SELECT
+    payment_date,
+    annual_interest_rate,
+    interest_amount,
+    remaining_balance,
+    LAG(remaining_balance)
+      OVER (ORDER BY payment_date) AS prev_remaining_balance
+  FROM stg_data
+),
+
+validated AS (
+  SELECT
+    payment_date,
+    annual_interest_rate,
+    prev_remaining_balance,
+    interest_amount,
+    FLOOR(
+      prev_remaining_balance * (annual_interest_rate / 100) / 12
+    ) AS expected_interest_amount
+  FROM
+    ordered
+  WHERE
+    prev_remaining_balance IS NOT NULL
+),
+
+invalid_balance_records AS (
+  SELECT
+    payment_date,
+    annual_interest_rate,
+    prev_remaining_balance,
+    interest_amount,
+    expected_interest_amount
+  FROM
+    validated
+  WHERE
+    interest_amount <> expected_interest_amount
+)
+
+SELECT * FROM invalid_balance_records
