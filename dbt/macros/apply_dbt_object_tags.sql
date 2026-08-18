ba889e1b4dae606b9552863e3ad63933fd709909
EC2 Default User <ec2-user@ip-10-120-10-102.ap-northeast-1.compute.internal>
2026-08-18 10:19:20 +0000

    バグ修正

--- dbt/macros/apply_dbt_object_tags.sql
+++ dbt/macros/apply_dbt_object_tags.sql
@@ -1,6 +1,6 @@
 {% macro apply_dbt_object_tags(results) %}
 
-  {% for result in results if result and result.get('node') %}
+  {% for result in results if result and result.node is defined %}
     
     {% set relation = adapter.get_relation(
       database=result.node.database,
@@ -11,7 +11,7 @@
     {# relationが存在し、かつ対象のタイプである場合だけSQLを作る #}
     {% if relation and relation.type | lower in ['table', 'view', 'materialized_view', 'dynamic_table', 'external_table'] %}
       {% set sql %}
-        ALTER {{ relation.type | upper }} {{ relation }} SET TAG OBJECT_MANAGED_BY = 'dbt';
+        ALTER {{ relation.type | upper }} {{ relation }} SET TAG COMMON_DB.GOVERNANCE.OBJECT_MANAGED_BY = 'dbt';
       {% endset %}
       {% do run_query(sql) %}
     {% endif %}
