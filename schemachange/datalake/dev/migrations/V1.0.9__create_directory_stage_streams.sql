f34e4f4dd40526d08dea9edea92e3e8c6ac874fc
yusuke-shudo-member <293005061+yusuke-shudo-member@users.noreply.github.com>
2026-09-11 02:33:23 +0000

    バグ修正

--- schemachange/datalake/dev/migrations/V1.0.9__create_directory_stage_streams.sql
+++ schemachange/datalake/dev/migrations/V1.0.9__create_directory_stage_streams.sql
@@ -3,7 +3,7 @@ CREATE STREAM datalake_db.paypay_bank.stream_on_stage_paypay_bank
   ON STAGE datalake_db.paypay_bank.stage_paypay_bank_stream_triggered
 ;
 
-CREATE TRANSIENT TABLE IF NOT EXISTS datalake_db.paypay_bank.work_from_stream_paypay_bank (
+CREATE TRANSIENT TABLE datalake_db.paypay_bank.work_from_stream_paypay_bank (
   relative_path  VARCHAR,
   action         VARCHAR,
   last_modified  TIMESTAMP_TZ
@@ -16,7 +16,7 @@ CREATE STREAM datalake_db.orico_credit.stream_on_stage_orico_credit
   ON STAGE datalake_db.orico_credit.stage_orico_credit_stream_triggered
 ;
 
-CREATE TRANSIENT TABLE IF NOT EXISTS datalake_db.orico_credit.work_from_stream_orico_credit (
+CREATE TRANSIENT TABLE datalake_db.orico_credit.work_from_stream_orico_credit (
   relative_path  VARCHAR,
   action         VARCHAR,
   last_modified  TIMESTAMP_TZ
@@ -26,10 +26,10 @@ WITH TAG (common_db.governance.object_managed_by = 'schemachange')
 
 CREATE STREAM datalake_db.sbi_securities.stream_on_stage_sbi_securities
   WITH TAG (common_db.governance.object_managed_by = 'schemachange')
-  ON STAGE datalake_db.orico_credit.stage_sbi_securities_stream_triggered
+  ON STAGE datalake_db.sbi_securities.stage_sbi_securities_stream_triggered
 ;
 
-CREATE TRANSIENT TABLE IF NOT EXISTS datalake_db.sbi_securities.work_from_stream_sbi_securities (
+CREATE TRANSIENT TABLE datalake_db.sbi_securities.work_from_stream_sbi_securities (
   relative_path  VARCHAR,
   action         VARCHAR,
   last_modified  TIMESTAMP_TZ
@@ -39,10 +39,10 @@ WITH TAG (common_db.governance.object_managed_by = 'schemachange')
 
 CREATE STREAM datalake_db.monex_securities.stream_on_stage_monex_securities
   WITH TAG (common_db.governance.object_managed_by = 'schemachange')
-  ON STAGE datalake_db.orico_credit.stage_monex_securities_stream_triggered
+  ON STAGE datalake_db.monex_securities.stage_monex_securities_stream_triggered
 ;
 
-CREATE TRANSIENT TABLE IF NOT EXISTS datalake_db.monex_securities.work_from_stream_monex_securities (
+CREATE TRANSIENT TABLE datalake_db.monex_securities.work_from_stream_monex_securities (
   relative_path  VARCHAR,
   action         VARCHAR,
   last_modified  TIMESTAMP_TZ
