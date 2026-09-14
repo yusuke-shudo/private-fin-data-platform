18cefaa937bd5c12613c2fa4500c4931d645100d
yusuke-shudo-member <293005061+yusuke-shudo-member@users.noreply.github.com>
2026-09-14 05:11:01 +0000

    バグ修正

--- schemachange/datawarehouse/dev/migrations/V1.0.0__stream_for_dbt_build_per_raw.sql
+++ schemachange/datawarehouse/dev/migrations/V1.0.0__stream_for_dbt_build_per_raw.sql
@@ -3,11 +3,11 @@
 -- Placed in DATAWAREHOUSE_DB.COMMON for centralized task orchestration
 
 CREATE STREAM IF NOT EXISTS "DATAWAREHOUSE_DB"."COMMON"."stream_for_dbt_build_paypay_home_loan"
-  ON TABLE datalake_db.paypay_bank.home_loan_schedule_raw
   WITH TAG (common_db.governance.object_managed_by = 'schemachange')
+  ON TABLE datalake_db.paypay_bank.home_loan_schedule_raw
 ;
 
 CREATE STREAM IF NOT EXISTS "DATAWAREHOUSE_DB"."COMMON"."stream_for_dbt_build_orico_home_reform"
-  ON TABLE datalake_db.orico_credit.home_reform_loan_schedule_raw
   WITH TAG (common_db.governance.object_managed_by = 'schemachange')
+  ON TABLE datalake_db.orico_credit.home_reform_loan_schedule_raw
 ;
