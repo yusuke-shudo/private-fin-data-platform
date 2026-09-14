c0a819f21fc3f5731fcff7968307fa2a1e228ffc
yusuke-shudo-member <293005061+yusuke-shudo-member@users.noreply.github.com>
2026-09-14 08:47:57 +0000

    修正

--- terraform/snowflake/platform/dev/dbt.tf
+++ terraform/snowflake/platform/dev/dbt.tf
@@ -2,7 +2,7 @@
 # DBT Engineer Role for Snowflake native dbt execution
 # ==============================================================================
 
-resource "snowflake_role" "dbt_engineer" {
+resource "snowflake_account_role" "dbt_engineer" {
   name    = "DBT_ENGINEER_ROLE"
   comment = "Role for dbt execution on Snowflake native DBT PROJECT | ${local.managed_comment}"
 }
